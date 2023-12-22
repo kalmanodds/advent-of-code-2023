5949395234729969ce5896668deffd04ae76b916
kalmanodds <kalman@tix.is>
2023-12-22 22:41:20 +0100

    Day 16 elixir

--- elixir/lib/day_16.ex
+++ elixir/lib/day_16.ex
@@ -1,76 +1,69 @@
 defmodule Day16 do
-  @moduledoc """
-  Upon closer inspection, the contraption appears to be a flat,
-  two-dimensional square grid containing
-  empty space (.), mirrors (/ and \), and splitters (| and -).
-
-  The contraption is aligned so that most of the beam bounces around the grid,
-  but each tile on the grid converts some of the beam's light into heat to melt the rock in the cavern.
-
-  You note the layout of the contraption (your puzzle input). For example:
-
-  .|...\....
-  |.-.\.....
-  .....|-...
-  ........|.
-  ..........
-  .........\
-  ..../.\\..
-  .-.-/..|..
-  .|....-|.\
-  ..//.|....
-  The beam enters in the top-left corner from the left and heading to the right.
-  Then, its behavior depends on what it encounters as it moves:
-
-  If the beam encounters empty space (.), it continues in the same direction.
-  If the beam encounters a mirror (/ or \), the beam is reflected 90 degrees depending on the angle of the mirror.
-    For instance, a rightward-moving beam that encounters a / mirror would continue upward in the mirror's column,
-    while a rightward-moving beam that encounters a \ mirror would continue downward from the mirror's column.
-  If the beam encounters the pointy end of a splitter (| or -),
-  the beam passes through the splitter as if the splitter were empty space.
-    For instance, a rightward-moving beam that encounters a - splitter would continue in the same direction.
-  If the beam encounters the flat side of a splitter (| or -),
-  the beam is split into two beams going in each of the two directions the splitter's pointy ends are pointing.
-    For instance, a rightward-moving beam that encounters a | splitter would split into two beams:
-    one that continues upward from the splitter's column and one that continues downward from the splitter's column.
-  Beams do not interact with other beams; a tile can have many beams passing through it at the same time.
-  A tile is energized if that tile has at least one beam pass through it, reflect in it, or split in it.
-
-  In the above example, here is how the beam of light bounces around the contraption:
-
-  >|<<<\....
-  |v-.\^....
-  .v...|->>>
-  .v...v^.|.
-  .v...v^...
-  .v...v^..\
-  .v../2\\..
-  <->-/vv|..
-  .|<<<2-|.\
-  .v//.|.v..
-  Beams are only shown on empty tiles; arrows indicate the direction of the beams.
-  If a tile contains beams moving in multiple directions, the number of distinct directions is shown instead.
-  Here is the same diagram but instead only showing whether a tile is energized (#) or not (.):
-
-  ######....
-  .#...#....
-  .#...#####
-  .#...##...
-  .#...##...
-  .#...##...
-  .#..####..
-  ########..
-  .#######..
-  .#...#.#..
-  Ultimately, in this example, 46 tiles become energized.
-
-  The light isn't energizing enough tiles to produce lava; to debug the contraption,
-  you need to start by analyzing the current situation.
-  With the beam starting in the top-left heading right, how many tiles end up being energized?
-  """
-
   def main(file_path) do
     {_, content} = File.read(file_path)
+    grid = content
+    |> String.split("\r\n")
+    |> Enum.map(&String.graphemes/1)
+    |> traverse_grid()
+    |> Map.keys()
+    |> Enum.count()
+    grid
+  end
 
+  def traverse_grid(grid, direction \\ :right, position \\ {0, 0}, state \\ %{}) do
+    if out_of_bounds?(grid, position) do
+      state
+    else
+      {x, y} = position
+      current_symbol = grid |> Enum.at(y) |> Enum.at(x)
+      combos = [
+        {direction, [{direction, "."}]},
+        {:up, [{:up, "|"}, {:left, "\\"}, {:right, "/"}]},
+        {:down, [{:down, "|"}, {:left, "/"}, {:right, "\\"}]},
+        {:left, [{:left, "-"}, {:up, "\\"}, {:down, "/"}]},
+        {:right, [{:right, "-"}, {:up, "/"}, {:down, "\\"}]},
+        {{:up, :down}, [{:left, "|"}, {:right, "|"}]},
+        {{:left, :right}, [{:up, "-"}, {:down, "-"}]}
+      ]
+      new_direction = combos |> Enum.find(fn {_, combos} ->
+        Enum.any?(combos, &(&1 === {direction, current_symbol}))
+      end)
+      case new_direction do
+        {{direction_1, direction_2}, _} ->
+          new_state = traverse_direction(grid, direction_1, position, state)
+          traverse_direction(grid, direction_2, position, new_state)
+        {new_direction, _} ->
+          traverse_direction(grid, new_direction, position, state)
+      end
+    end
+  end
+
+  def traverse_direction(grid, direction, position, state) do
+    if Map.fetch(state, position) === {:ok, direction} do
+      state
+    else
+      new_state = state |> Map.put(position, direction)
+      new_position = update_position(position, direction)
+      traverse_grid(grid, direction, new_position, new_state)
+    end
+  end
+
+  def out_of_bounds?(grid, {x, y}) do
+    x_length = grid |> Enum.at(0) |> Enum.count()
+    y_length = grid |> Enum.count()
+    [x, y]
+    |> Enum.zip([x_length, y_length])
+    |> Enum.any?(fn ({position, length}) ->
+      position < 0 || position >= length
+    end)
+  end
+
+  def update_position({x, y}, direction) do
+    case direction do
+      :up -> {x, y - 1}
+      :down -> {x, y + 1}
+      :left -> {x - 1, y}
+      :right -> {x + 1, y}
+    end
   end
 end
--- elixir/test/day_16_test.exs
+++ elixir/test/day_16_test.exs
@@ -5,6 +5,16 @@ defmodule Day16Test do
     assert Day16.main("inputs/day_16_example.txt") === 46
   end
 
+  test "out_of_bounds? should pass" do
+    assert Day16.out_of_bounds?([[]], {-1, 0}) === true
+  end
+
+  test "traverse_grid should pass a simple dot grid" do
+    grid = [[".", "."], [".", "."]]
+    expected = %{{0, 0} => :right, {1, 0} => :right}
+    assert Day16.traverse_grid(grid) === expected
+  end
+
   test "Day 16 input should pass" do
     assert Day16.main("inputs/day_16_input.txt") === 0
   end
