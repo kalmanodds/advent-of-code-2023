46c00b0fa317f63e9e9103fffe0a18f2f72dea59
kalmanodds <kalman@tix.is>
2023-12-26 14:44:23 +0100

    Day 18 w shoelace algorithm

--- elixir/lib/day_18.ex
+++ elixir/lib/day_18.ex
@@ -8,30 +8,25 @@ defmodule Day18 do
       {direction, String.to_integer(count)}
     end)
 
-    dimensions = instructions
-    |> find_dimensions()
+    {vertices, sum} = instructions |> get_vertices_and_perimeter()
+    area = vertices |> compute_area()
 
-    edge_count = instructions
-    |> create_coords_list()
-    |> count_edges(dimensions)
-
-    [x_length, y_length] = dimensions
-    (x_length + 1) * (y_length + 1) - edge_count
+    area + sum
   end
 
-  def find_dimensions(instructions) do
-    {list, _} = instructions
-    |> Enum.map_reduce({0, 0}, fn (instruction, {x, y}) ->
-      {direction, count} = instruction
-      new_acc = increment_coords({x, y}, direction, count)
-      {new_acc, new_acc}
-    end)
-
-    {x_list, y_list} = list |> Enum.unzip()
-    [x_list, y_list] |> Enum.map(&Enum.max/1)
+  def get_vertices_and_perimeter(instructions) do
+    vertex = {0, 0}
+    {_, vertices, sum} = instructions
+    |> Enum.reduce({vertex, [vertex], 2},
+      fn (instruction, {vertex, vertices, sum}) ->
+        {direction, count} = instruction
+        new_vertex = update_vertex(vertex, direction, count)
+        {new_vertex, [new_vertex | vertices], sum + count}
+      end)
+    {vertices, sum |> div(2)}
   end
 
-  def increment_coords({x, y}, direction, increment \\ 1) do
+  def update_vertex({x, y}, direction, increment \\ 1) do
     case direction do
       "R" -> {x + increment, y}
       "L" -> {x - increment, y}
@@ -40,63 +35,14 @@ defmodule Day18 do
     end
   end
 
-  def create_coords_list(instructions) do
-    coords = {0, 0}
-    {_, coords_list} = instructions
-    |> Enum.reduce({coords, [coords]}, fn (instruction, {coords, coords_list}) ->
-      {direction, count} = instruction
-      new_coords_list = (1..count)
-      |> Enum.to_list()
-      |> Enum.map(&increment_coords(coords, direction, &1))
-      {List.last(new_coords_list), coords_list ++ new_coords_list}
+  def compute_area([{x1, y1} | rest]) do
+    {area, _} = rest
+    |> Enum.reduce({0, {x1, y1}}, fn {x2, y2}, {acc, {x1, y1}} ->
+      product1 = x1 * y2
+      product2 = x2 * y1
+      new_acc = acc + product1 - product2
+      {new_acc, {x2, y2}}
     end)
-    coords_list
-  end
-
-  def count_edges([coords | coords_list], dimensions) do
-    {_, _, edge_count} = coords_list
-    |> Enum.reduce({coords, coords_list, 0}, fn (coords, acc) ->
-      {last_coords, coords_list, edge_count} = acc
-      direction = determine_direction(last_coords, coords)
-      new_coords = increment_coords(coords, direction)
-      new_acc = {coords, coords_list, edge_count}
-      if within_range?(new_coords, dimensions) do
-        count_edge(new_coords, new_acc, dimensions)
-      else
-        new_acc
-      end
-    end)
-    edge_count
-  end
-
-  def within_range?({x, y}, dimensions) do
-    [x_length, y_length] = dimensions
-    x >= 0 && x <= x_length && y >= 0 && y <= y_length
-  end
-
-  def determine_direction({x1, y1}, {x2, y2}) do
-    cond do
-      x1 < x2 -> "U" # right
-      x1 > x2 -> "D" # left
-      y1 < y2 -> "R" # down
-      y1 > y2 -> "L" # up
-    end
-  end
-
-  def count_edge(coords, acc, dimensions) do
-    {last_coords, coords_list, edge_count} = acc
-    if Enum.member?(coords_list, coords) do
-      acc
-    else
-      new_coords_list = [coords | coords_list]
-      new_count = edge_count + 1
-      new_acc = {last_coords, new_coords_list, new_count}
-      ["R", "L", "U", "D"]
-      |> Enum.map(&increment_coords(coords, &1))
-      |> Enum.filter(&within_range?(&1, dimensions))
-      |> Enum.reduce(new_acc, fn (coords, acc) ->
-        count_edge(coords, acc, dimensions)
-      end)
-    end
+    area |> abs() |> div(2)
   end
 end
